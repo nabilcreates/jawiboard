324edaff704bd1035cd652362fa3493abdc25318
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-15 00:13:25 +0800

    initial payload

---
+++ main.js
@@ -0,0 +1,42 @@
+class App extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            letters:['ا','ب','ت','ث','ج','چ','ح','خ','د','ذ','ر','ز','س','ش','ص','ض','ط','ظ','ع','غ','ڠ','ف','ڤ','ق','ک','ݢ','ل','م','ن','و','ۏ','ه','ة','ء','ي','ڽ','ى', ' '],
+            value: '',
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e){
+        let letter = e.target.innerText
+        console.log(letter)
+        this.setState(prev => {
+            return{
+                value: prev.value.concat(letter)
+            }
+        })
+    }
+    
+    render(){
+        return(
+            <div>
+
+                <input id='arab_input' value={this.state.value} ></input>
+                
+                <div id='buttons'>
+                    {this.state.letters.map(letter => {
+                    return <p value={letter} onClick={this.handleClick} id='arab'>{letter}</p>})}
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
